a6408a458d0d7d1320614691ddf21b600f4f65b9
renatinhafront <re.saraiva09@hotmail.com>
2024-02-22 13:25:59 -0300

    alterado id

--- src/data/dataset.js
+++ src/data/dataset.js
@@ -1,6 +1,6 @@
 export default [
   {
-    "id": "the-shawshank-redemption",
+    "id": "1",
     "name": "Um Sonho de Liberdade",
     "shortDescription": "Prisioneiro busca liberdade em Um Sonho de Liberdade.",
     "description": "Um banqueiro injustamente condenado à prisão busca a redenção através de uma amizade inesperada com um colega de cela. Este épico drama, dirigido por Frank Darabont, explora a esperança, a amizade e a busca pela liberdade. Ambientado em Shawshank, uma prisão sombria, o filme mergulha nos desafios enfrentados pelo protagonista, Andy Dufresne, enquanto ele enfrenta injustiças, forja conexões inesperadas e mantém a esperança contra todas as probabilidades. Com uma impressionante pontuação de 9.3 no IMDb, Um Sonho de Liberdade permanece como um testemunho poderoso da resiliência humana e da busca pela verdadeira redenção.",
@@ -15,7 +15,7 @@ export default [
     }
   },
   {
-    "id": "the-godfather",
+    "id": "2",
     "name": "O Poderoso Chefão",
     "shortDescription": "Família mafiosa liderada por Vito Corleone em O Poderoso Chefão.",
     "description": "O Poderoso Chefão, dirigido por Francis Ford Coppola, é uma jornada monumental através do submundo da máfia italiana. Lançado em 1972, o filme segue Michael Corleone, interpretado por Al Pacino, enquanto ele é gradualmente envolvido nos intricados negócios da família após eventos violentos. Com uma pontuação de 9.2 no IMDb, O Poderoso Chefão é uma obra-prima aclamada por suas performances excepcionais, roteiro brilhante e cinematografia marcante. A narrativa rica e complexa explora temas de lealdade, poder e tragédia, solidificando seu lugar como um dos maiores filmes da história do cinema.",
@@ -30,7 +30,7 @@ export default [
     }
   },
   {
-    "id": "the-dark-knight",
+    "id": "3",
     "name": "Batman - O Cavaleiro das Trevas",
     "shortDescription": "Batman enfrenta o Coringa em Batman - O Cavaleiro das Trevas.",
     "description": "Batman - O Cavaleiro das Trevas, dirigido por Christopher Nolan, transcende o gênero de super-heróis, oferecendo uma experiência cinematográfica inigualável. Lançado em 2008, o filme mergulha no caos instaurado pelo lendário Coringa, interpretado magistralmente por Heath Ledger, enquanto Batman, interpretado por Christian Bale, enfrenta dilemas morais e desafios existenciais. Com uma pontuação de 9.0 no IMDb, O Cavaleiro das Trevas é uma obra-prima que redefine as expectativas do gênero, explorando temas de moralidade, caos e a dualidade humana de maneiras profundas e inovadoras.",
@@ -45,7 +45,7 @@ export default [
     }
   },
   {
-    "id": "the-godfather-ii",
+    "id": "4",
     "name": "O Poderoso Chefão II",
     "shortDescription": "Ascensão de Michael Corleone em O Poderoso Chefão II.",
     "description": "O Poderoso Chefão II, dirigido por Francis Ford Coppola, é uma obra-prima cinematográfica que continua a saga da família Corleone. Lançado em 1974, o filme entrelaça as histórias de Michael Corleone, interpretado por Al Pacino, e seu pai Vito, interpretado por Robert De Niro, explorando a ascensão ao poder e o legado da família. Com uma pontuação de 9.0 no IMDb, O Poderoso Chefão II é elogiado por suas performances excepcionais, narrativa complexa e impacto duradouro na história do cinema.",
@@ -60,7 +60,7 @@ export default [
     }
   },
   {
-    "id": "12-angry-men",
+    "id": "5",
     "name": "12 Homens e uma Sentença",
     "shortDescription": "Deliberação de júri em caso de homicídio em 12 Homens e uma Sentença.",
     "description": "12 Homens e uma Sentença, dirigido por Sidney Lumet, é um drama judicial intenso que se desenrola em uma sala de deliberação. Lançado em 1957, o filme examina o julgamento de um jovem acusado de assassinato, enquanto doze jurados confrontam suas próprias dúvidas e preconceitos. Com uma pontuação de 9.0 no IMDb, 12 Homens e uma Sentença é uma obra-prima atemporal que explora a complexidade da verdade, moralidade e responsabilidade individual, oferecendo uma reflexão profunda sobre o sistema judicial e a natureza humana.",
@@ -75,7 +75,7 @@ export default [
     }
   },
   {
-    "id": "schindlers-list",
+    "id": "6",
     "name": "A Lista de Schindler",
     "shortDescription": "Salvamento de judeus durante o Holocausto em A Lista de Schindler.",
     "description": "A Lista de Schindler, dirigido por Steven Spielberg, é uma obra-prima emocional e impactante que narra a história verídica de Oskar Schindler durante o Holocausto. Lançado em 1993, o filme retrata a jornada do empresário alemão que salva a vida de mais de mil judeus. Com uma pontuação de 8.9 no IMDb, A Lista de Schindler é um testemunho comovente de coragem, humanidade e redenção em meio à brutalidade da guerra. As performances notáveis, a trilha sonora evocativa e a direção magistral solidificam seu lugar como uma das narrativas mais poderosas já contadas no cinema.",
@@ -90,7 +90,7 @@ export default [
     }
   },
   {
-    "id": "tlotr-return-of-the-king",
+    "id": "7",
     "name": "O Senhor dos Anéis: O Retorno do Rei",
     "shortDescription": "Frodo completa jornada para destruir anel em O Senhor dos Anéis: O Retorno do Rei.",
     "description": "O Senhor dos Anéis: O Retorno do Rei conclui a épica trilogia dirigida por Peter Jackson. Na batalha final pela Terra Média, Frodo e Sam continuam a jornada para destruir o Um Anel enquanto Aragorn lidera as forças da luz contra Sauron. O filme é uma obra-prima cinematográfica, repleta de batalhas grandiosas, emoções intensas e momentos épicos. Com uma narrativa envolvente e personagens icônicos, esta conclusão épica da trilogia de J.R.R. Tolkien cativa o público com sua magia, coragem e a eterna luta entre o bem e o mal.",
@@ -105,7 +105,7 @@ export default [
     }
   },
   {
-    "id": "pulp-fiction",
+    "id": "8",
     "name": "Pulp Fiction - Tempo de Violência",
     "shortDescription": "Histórias interligadas de criminosos em Pulp Fiction - Tempo de Violência.",
     "description": "Pulp Fiction - Tempo de Violência, dirigido por Quentin Tarantino, é uma obra-prima do cinema contemporâneo. Lançado em 1994, o filme entrelaça histórias de gangsters, boxeadores e assassinos de aluguel de maneira única. Com uma pontuação de 8.9 no IMDb, Pulp Fiction é conhecido por seu diálogo afiado, reviravoltas surpreendentes e trilha sonora memorável. A narrativa não linear e as performances marcantes, incluindo a icônica dança de Mia Wallace, tornam este filme uma experiência cinematográfica essencial que desafia convenções e define um estilo ousado e inovador.",
@@ -120,7 +120,7 @@ export default [
     }
   },
   {
-    "id": "tlotr-fellowship-of-the-ring",
+    "id": "9",
     "name": "O Senhor dos Anéis: A Sociedade do Anel",
     "shortDescription": "A jornada épica da Sociedade do Anel para destruir Um Anel.",
     "description": "O Senhor dos Anéis: A Sociedade do Anel inicia a trilogia épica de Peter Jackson, adaptada da obra de J.R.R. Tolkien. A trama segue Frodo, um hobbit, e um grupo diversificado de personagens, incluindo Aragorn e Gandalf, que formam a Sociedade do Anel para destruir o perigoso Um Anel. A jornada leva-os através de paisagens deslumbrantes e confronta-os com ameaças mortais. Com uma narrativa rica, efeitos visuais espetaculares e a introdução do mundo fantástico da Terra Média, o filme estabelece um padrão elevado para a trilogia, cativando o público com uma história de coragem e amizade.",
@@ -136,7 +136,7 @@ export default [
     }
   },
   {
-    "id": "the-good-the-bad-and-the-ugly",
+    "id": "10",
     "name": "Três Homens em Conflito",
     "shortDescription": "Conflitos intensos em busca de um tesouro enterrado.",
     "description": "Três Homens em Conflito, dirigido por Sergio Leone, é um clássico do western spaghetti que se destaca pela narrativa envolvente e duelos épicos. Lançado em 1966, o filme segue três pistoleiros em busca de um tesouro enterrado durante a Guerra Civil Americana. Com uma pontuação de 8.8 no IMDb, Três Homens em Conflito é um épico cinematográfico que combina trama cativante, personagens memoráveis e uma trilha sonora icônica. A direção magistral de Leone e as performances marcantes consolidam este filme como um marco no gênero western.",
@@ -151,7 +151,7 @@ export default [
     }
   },
   {
-    "id": "forrest-gump",
+    "id": "11",
     "name": "Forrest Gump: O Contador de Histórias",
     "shortDescription": "A extraordinária vida de Forrest Gump, um contador de histórias incomum.",
     "description": "Forrest Gump: O Contador de Histórias, dirigido por Robert Zemeckis, é uma emocionante jornada através das décadas da história americana, vista pelos olhos de Forrest Gump. Lançado em 1994, o filme é uma mistura única de drama, comédia e reflexão sobre a vida. Com uma pontuação de 8.8 no IMDb, Forrest Gump é conhecido por sua narrativa cativante, performances notáveis de Tom Hanks e efeitos visuais inovadores. A história sincera e otimista de Forrest continua a encantar espectadores de todas as idades, tornando-se um clássico contemporâneo.",
@@ -166,7 +166,7 @@ export default [
     }
   },
   {
-    "id": "fight-club",
+    "id": "12",
     "name": "Clube da Luta",
     "shortDescription": "Descontentamento social leva a lutas clandestinas e uma busca pela identidade.",
     "description": "Clube da Luta, dirigido por David Fincher, é uma exploração arrojada da psique humana. Lançado em 1999, o filme segue um homem desencantado, interpretado por Edward Norton, que forma um clube secreto de luta com o carismático Tyler Durden, interpretado por Brad Pitt. Com uma pontuação de 8.8 no IMDb, Clube da Luta é conhecido por suas reviravoltas chocantes, questionamentos existenciais e comentários sociais. A narrativa intensa e a cinematografia distinta solidificam este filme como um marco cultural que continua a desafiar as convenções e a provocar reflexões profundas.",
@@ -181,7 +181,7 @@ export default [
     }
   },
   {
-    "id": "tlotr-two-towers",
+    "id": "13",
     "name": "O Senhor dos Anéis: As Duas Torres",
     "shortDescription": "A continuação da épica jornada da Sociedade do Anel.",
     "description": "O Senhor dos Anéis: As Duas Torres, dirigido por Peter Jackson, é a continuação emocionante da trilogia. Lançado em 2002, o filme mergulha nas jornadas paralelas de Frodo e seus companheiros enquanto a Terra Média enfrenta crescentes ameaças das forças das trevas. Com uma pontuação de 8.7 no IMDb, As Duas Torres é uma obra-prima visual com batalhas grandiosas, personagens complexos e uma narrativa que mantém os espectadores à beira de seus assentos, solidificando a trilogia como um épico inigualável.",
@@ -196,7 +196,7 @@ export default [
     }
   },
   {
-    "id": "inception",
+    "id": "14",
     "name": "A Origem",
     "shortDescription": "A exploração de sonhos e a complexidade da realidade.",
     "description": "A Origem, dirigido por Christopher Nolan, é uma jornada cerebral de intriga e ação. Lançado em 2010, o filme segue Dom Cobb, interpretado por Leonardo DiCaprio, em uma missão para implantar uma ideia na mente de alguém através dos sonhos. Com uma pontuação de 8.8 no IMDb, A Origem é elogiado por suas sequências de ação inovadoras, narrativa intricada e performances excepcionais. A exploração da realidade distorcida e a busca pela verdade fazem deste filme uma experiência cinematográfica envolvente e inesquecível.",
@@ -211,7 +211,7 @@ export default [
     }
   },
   {
-    "id": "the-empire-strikes-back",
+    "id": "15",
     "name": "Star Wars: Episódio V - O Império Contra-Ataca",
     "shortDescription": "Os rebeldes enfrentam o Império Galáctico em uma batalha épica.",
     "description": "Star Wars: Episódio V - O Império Contra-Ataca é a segunda parte da trilogia original de George Lucas. A saga continua com a Aliança Rebelde em fuga enquanto Darth Vader intensifica seus esforços para esmagar a rebelião. O filme destaca a jornada de Luke Skywalker em treinamento Jedi sob Yoda, enquanto Han Solo enfrenta desafios. Com reviravoltas surpreendentes, batalhas épicas e o impactante confronto entre Luke e Vader, 'O Império Contra-Ataca' é amplamente considerado o ápice da saga, oferecendo uma narrativa rica, desenvolvimento de personagens profundo e um final impactante que deixa os fãs ansiosos pela conclusão.",
@@ -226,7 +226,7 @@ export default [
     }
   },
   {
-    "id": "the-matrix",
+    "id": "16",
     "name": "Matrix",
     "shortDescription": "Neo descobre a verdade sobre a realidade em um mundo controlado por máquinas.",
     "description": "Matrix, dirigido pelos irmãos Wachowski, é um marco na Syfy que desafia a percepção da realidade. Lançado em 1999, o filme segue Neo, interpretado por Keanu Reeves, enquanto ele descobre a verdade por trás da realidade virtual que o aprisiona. Com uma pontuação de 8.7 no IMDb, Matrix é conhecido por seus efeitos visuais inovadores, sequências de ação memoráveis e complexidade filosófica. A narrativa envolvente e a influência duradoura solidificam Matrix como uma obra-prima moderna e um divisor de águas no cinema.",
@@ -241,7 +241,7 @@ export default [
     }
   },
   {
-    "id": "goodfellas",
+    "id": "17",
     "name": "Os Bons Companheiros",
     "shortDescription": "Crime, lealdade e ascensão no submundo.",
     "description": "Os Bons Companheiros, dirigido por Martin Scorsese, é um épico do crime que mergulha na ascensão e queda de gangsters. Lançado em 1990, o filme narra a vida de Henry Hill, interpretado por Ray Liotta, dentro da máfia italiana. Com uma pontuação de 8.7 no IMDb, Os Bons Companheiros destaca-se por sua narrativa frenética, performances excepcionais e a visão distintiva de Scorsese sobre o mundo do crime. A lealdade, a traição e as consequências são exploradas de maneira impactante, solidificando este filme como um clássico do gênero.",
@@ -256,7 +256,7 @@ export default [
     }
   },
   {
-    "id": "one-flew-over-the-cuckoos-nest",
+    "id": "18",
     "name": "Um Estranho no Ninho",
     "shortDescription": "Conflitos em um hospital psiquiátrico liderados por um rebelde.",
     "description": "Um Estranho no Ninho, dirigido por Milos Forman, é uma poderosa exploração da sanidade e da rebelião contra a conformidade. Lançado em 1975, o filme segue Randle McMurphy, interpretado por Jack Nicholson, enquanto ele desafia a autoridade em uma instituição mental. Com uma pontuação de 8.7 no IMDb, Um Estranho no Ninho é reconhecido por suas performances impactantes, especialmente de Nicholson, e pela reflexão profunda sobre a natureza humana. A narrativa habilmente construída e as questões sociais exploradas fazem deste filme um clássico duradouro e provocativo.",
@@ -271,7 +271,7 @@ export default [
     }
   },
   {
-    "id": "se7en",
+    "id": "19",
     "name": "Seven: Os Sete Crimes Capitais",
     "shortDescription": "Detetives perseguem um assassino em série em um thriller psicológico.",
     "description": "Seven: Os Sete Crimes Capitais, dirigido por David Fincher, é um thriller psicológico arrebatador. Lançado em 1995, o filme segue os detetives David Mills e William Somerset, interpretados por Brad Pitt e Morgan Freeman, em uma caçada angustiante a um serial killer que opera com base nos sete pecados capitais. Com uma pontuação de 8.6 no IMDb, Seven é elogiado por sua atmosfera sombria, reviravoltas impactantes e performances excepcionais. A exploração profunda da natureza humana e a narrativa envolvente solidificam este filme como um clássico do suspense moderno.",
@@ -286,7 +286,7 @@ export default [
     }
   },
   {
-    "id": "its-a-wonderful-life",
+    "id": "20",
     "name": "A Felicidade Não se Compra",
     "shortDescription": "Um anjo ajuda um homem a valorizar a vida em uma jornada emocional.",
     "description": "A Felicidade Não se Compra, dirigido por Frank Capra, é um clássico atemporal que explora o significado da vida. Lançado em 1946, o filme segue George Bailey, interpretado por James Stewart, enquanto ele reflete sobre sua existência com a ajuda de um anjo. Com uma pontuação de 8.6 no IMDb, A Felicidade Não se Compra é conhecido por sua mensagem inspiradora, performances cativantes e impacto emocional duradouro. A narrativa envolvente e a direção magistral fazem deste filme uma celebração do espírito humano e um símbolo do poder do bem sobre o mal.",
@@ -301,7 +301,7 @@ export default [
     }
   },
   {
-    "id": "seven-samurai",
+    "id": "21",
     "name": "Os Sete Samurais",
     "shortDescription": "Aldeões contratam samurais para proteger sua vila em um épico clássico.",
     "description": "Os Sete Samurais, dirigido por Akira Kurosawa, é um épico japonês que redefine o cinema de samurais. Lançado em 1954, o filme segue um grupo de samurais contratados para defender uma aldeia indefesa contra bandidos. Com uma pontuação de 8.6 no IMDb, Os Sete Samurais é aclamado por suas cenas de batalha magistrais, personagens complexos e exploração profunda de temas como honra e sacrifício. A influência duradoura e a maestria técnica tornam este filme uma referência essencial no mundo do cinema.",
@@ -316,7 +316,7 @@ export default [
     }
   },
   {
-    "id": "interstellar",
+    "id": "22",
     "name": "Interestelar",
     "shortDescription": "Uma viagem interestelar para salvar a humanidade.",
     "description": "Interestelar, dirigido por Christopher Nolan, é uma odisseia espacial emocionante. Em um futuro distópico, a Terra enfrenta uma crise agrícola, e um grupo de astronautas liderado por Cooper parte em uma missão interestelar para encontrar um novo lar para a humanidade. O filme explora temas de amor, tempo e sobrevivência, enquanto a equipe enfrenta desafios cósmicos e buracos de minhoca. Com visuais deslumbrantes, trilha sonora envolvente de Hans Zimmer e uma trama intricada que desafia a mente, Interestelar cativa o público, combinando ciência e emoção em uma experiência cinematográfica extraordinária.",
@@ -331,7 +331,7 @@ export default [
     }
   },
   {
-    "id": "the-silence-of-the-lambs",
+    "id": "23",
     "name": "O Silêncio dos Inocentes",
     "shortDescription": "Um agente do FBI busca por um serial killer com a ajuda de uma psiquiatra.",
     "description": "O Silêncio dos Inocentes, dirigido por Jonathan Demme, é um thriller psicológico que deixa o público à beira do assento. Lançado em 1991, o filme segue a agente do FBI Clarice Starling, interpretada por Jodie Foster, enquanto ela busca a ajuda do brilhante, mas perturbador, Dr. Hannibal Lecter, interpretado por Anthony Hopkins, para capturar um serial killer. Com uma pontuação de 8.6 no IMDb, O Silêncio dos Inocentes é elogiado por suas performances intensas, atmosfera tensa e reviravoltas impactantes. A narrativa sombria e a abordagem visceral consolidam este filme como um clássico do gênero.",
@@ -346,7 +346,7 @@ export default [
     }
   },
   {
-    "id": "saving-private-ryan",
+    "id": "24",
     "name": "O Resgate do Soldado Ryan",
     "shortDescription": "A busca por um soldado durante a Segunda Guerra Mundial.",
     "description": "O Resgate do Soldado Ryan, dirigido por Steven Spielberg, é uma representação visceral e comovente da Segunda Guerra Mundial. Lançado em 1998, o filme segue o Capitão Miller, interpretado por Tom Hanks, em uma missão para encontrar e resgatar o Soldado James Ryan, interpretado por Matt Damon. Com uma pontuação de 8.6 no IMDb, O Resgate do Soldado Ryan é conhecido por suas cenas de batalha realistas, performances emocionantes e reflexão sobre o sacrifício humano. A narrativa poderosa e a direção habilidosa de Spielberg tornam este filme uma homenagem duradoura aos que lutaram durante o conflito.",

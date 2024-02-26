a475211e1a3087a0104aa9799f8431bbc977ce54
renatinhafront <re.saraiva09@hotmail.com>
2024-02-26 08:35:43 -0300

    media add

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,27 +1,57 @@
-// export const home = (props) => {
-//   const element = document.createElement('div');
-//   element.textContent = `Bem vindo a página de início ${props.name}!`;
-//   return element;
-// }
-
 import dataMovie from '../data/dataset.js';
-import { sortData, totalMovies, filterData } from "../lib/dataFunctions.js"
-const titlesLength = document.querySelector(".titles_length");
-
-let movieData = [...dataMovie];
-
-const order = document.getElementById("order");
-const root = document.getElementById("root");
-const filters = document.getElementById("filters");
-const btnLimpar = document.getElementById("btn-limpar");
+import { sortData, filterData, totalMovies } from "../lib/dataFunctions.js";
 
 export function Home() {
+  let currentData = dataMovie;
   const viewEl = document.createElement('div');
-  viewEl.appendChild(renderSubTitle())
-  viewEl.appendChild(renderFilter())
-  viewEl.appendChild(renderItems(dataMovie))
+
+  const { subTitle, titlesLength } = renderSubTitle();
+  viewEl.appendChild(subTitle);
+
+  const { filter, order, filters, btnLimpar } = renderFilter();
+  viewEl.appendChild(filter);
+
+  const itemsContainer = document.createElement('div');
+  itemsContainer.id = "items-container";
+  viewEl.appendChild(itemsContainer);
+  itemsContainer.appendChild(renderItems(currentData));
+
+  titlesLength.innerText = totalMovies(currentData)
+
+  order.addEventListener("change", (element) => {
+    const orderValue = element.target.value;
+    currentData = sortData(currentData, "imDbRating", orderValue);
+    renderItemsInView(currentData);
+    titlesLength.innerText = totalMovies(currentData)
+  });
+
+  filters.addEventListener("change", (element) => {
+    const value = element.target.value;
+    if (value === "Todos") {
+      currentData = dataMovie;
+    } else {
+      currentData = filterData(dataMovie, "movieGender", value);
+    }
+    renderItemsInView(currentData);
+    titlesLength.innerText = totalMovies(currentData)
+  });
+
+  btnLimpar.addEventListener("click", () => {
+    currentData = dataMovie;
+    filters.value = "Todos";
+    order.value = "todos";
+    renderItemsInView(currentData);
+    titlesLength.innerText = totalMovies(currentData)
+  });
+
+  function renderItemsInView(data) {
+    itemsContainer.innerHTML = "";
+    itemsContainer.appendChild(renderItems(data));
+  }
+
   return viewEl;
 }
+
 export default Home;
 
 export const renderItems = (dataMovie) => {
@@ -29,22 +59,30 @@ export const renderItems = (dataMovie) => {
   ul.classList.add('container__card');
 
   dataMovie.forEach((item) => {
-    ul.innerHTML += `
-      <li itemscope itemtype="OsMelhoresFilmes" class="container__card">
-        <div class="content__card">
-          <dl itemscope itemtype="#">
-            <dt><img src="${item.imageUrl}" alt="Imagem do Filme" itemprop="${item.name}" class="image__card" /></dt>
-            <dd itemprop="sort-order" class="imDbRating">${item.facts.imDbRating} /10 <img src="./img/icon-star.svg" alt="Star icon" /></dd>
-            <dd itemprop="name" class="name__card">${item.name}</dd>
-            <dd itemprop="releaseYear" class="releaseYear">${item.facts.releaseYear} - ${item.facts.runtimeStr}</dd>
-            <!--<dd itemprop="runtimeStr" class="runtimeStr">${item.facts.runtimeStr}</dd>-->
-            <dd itemprop="shortDescription" class="shortDescription">${item.shortDescription}</dd>
-            <dd itemprop="movieGender" class="movieGender">${item.facts.movieGender}</dd>
-            <button class='btn-verMais'><dt>Ver mais</dt><dd itemprop="verMais"></dd></button>
-          </dl>
-        </div>
-      </li>
+    const li = document.createElement('li');
+    li.setAttribute('itemscope', '');
+    li.setAttribute('itemtype', 'OsMelhoresFilmes');
+    li.classList.add('container__card');
+
+    li.innerHTML = `
+      <div class="content__card">
+        <dl itemscope itemtype="#">
+          <dt>
+            <img src="${item.imageUrl}" alt="Imagem do Filme" itemprop="${item.name}" class="image__card" />
+          </dt>
+          <dd itemprop="sort-order" class="imDbRating">${item.facts.imDbRating} /10 <img src="./img/icon-star.svg" alt="Star icon" /></dd>
+          <dd itemprop="name" class="name__card">${item.name}</dd>
+          <dd itemprop="releaseYear" class="releaseYear">${item.facts.releaseYear} - ${item.facts.runtimeStr}</dd>
+          <dd itemprop="shortDescription" class="shortDescription">${item.shortDescription}</dd>
+          <dd itemprop="movieGender" class="movieGender">${item.facts.movieGender}</dd>
+          <button class='btn-verMais'>
+            <dt>Ver mais</dt>
+            <dd itemprop="verMais"></dd>
+          </button>
+        </dl>
+      </div>
     `;
+    ul.appendChild(li);
   });
 
   return ul;
@@ -54,7 +92,7 @@ export const renderFilter = () => {
   const divFilter = document.createElement('div');
   divFilter.classList.add('filtros');
 
-  divFilter.innerHTML += `
+  divFilter.innerHTML = `
       <section class="section-search">
           <label for="filters" id="search-filters" class="filters">Filtrar por:</label>
           <select id="filters" name="search-filter" data-testid="select-filter">
@@ -79,58 +117,31 @@ export const renderFilter = () => {
         <button id="btn-limpar" data-testid="button-clear">Limpar Filtros</button>
     </section>
       `;
-  return divFilter;
+
+  const order = divFilter.querySelector("#order");
+  const filters = divFilter.querySelector("#filters");
+  const btnLimpar = divFilter.querySelector("#btn-limpar");
+
+  return { filter: divFilter, order, filters, btnLimpar };
 };
 
 export const renderSubTitle = () => {
   const divSubTitle = document.createElement('div');
   divSubTitle.classList.add('container__text');
 
-  divSubTitle.innerHTML += `
+  divSubTitle.innerHTML = `
       <h1>Principais escolhas</h1>
       <h2>O que assistir</h2>
       <p>Os melhores filmes para você assistir.</p>
       <span class="titles_length"> títulos</span>
-      <button type="button" id="key">Key API</button>
       `;
-  return divSubTitle;
+
+  const titlesLength = divSubTitle.querySelector(".titles_length");
+  return { subTitle: divSubTitle, titlesLength};
 };
 
-order.addEventListener("change", (e) => {
-  const orderValue = e.target.value;
-  movieData = sortData(movieData, "imDbRating", e.target.value);
-  if (orderValue === "asc") {
-    movieData = sortData(movieData, "imDbRating", "asc");
-  }
-  else if (orderValue === "desc") {
-    movieData = sortData(movieData, "imDbRating", "desc");
-  }
-  root.innerHTML = "";
-  root.appendChild(renderItems(movieData));
-})
-
-filters.addEventListener("change", (e) => {
-  const value = e.target.value;
-  if (value === "Todos") {
-    movieData = [...dataMovie];
-  } else {
-    movieData = filterData(dataMovie, "movieGender", value);
-  }
-  root.innerHTML = "";
-  totalMovies(movieData, titlesLength);
-  root.appendChild(renderItems(movieData));
-})
-
-btnLimpar.addEventListener("click", () => {
-  movieData = [...dataMovie];
-  totalMovies(dataMovie);
-  root.innerHTML = "";
-  filters.value = "Todos";
-  order.value = "todos";
-  root.appendChild(renderItems(dataMovie));
-})
-
 document.addEventListener("DOMContentLoaded", () => {
-  totalMovies(movieData, titlesLength);
-  root.appendChild(renderItems(movieData));
-})
+  const homeView = Home();
+  const root = document.getElementById("root");
+  root.appendChild(homeView);
+});

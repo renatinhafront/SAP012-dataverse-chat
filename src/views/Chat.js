12c1e16540614908484570479d439cb8dcc76887
renatinhafront <re.saraiva09@hotmail.com>
2024-02-28 15:25:54 -0300

    estilizaçao pag chat

--- src/views/Chat.js
+++ src/views/Chat.js
@@ -5,54 +5,64 @@ export function Chat(props) {
   // if (props.id) {
   //   navigateTo("/error");
   // }
-  const divChat = document.createElement('div');
-  divChat.classList.add('detail__initial');
-  divChat.textContent = `Iniciar Conversa!`;
-
-  const inputChatText = document.createElement('textarea');
-  inputChatText.classList.add('detail__inputChatText');
-  inputChatText.readOnly = true;
-  inputChatText.cols = 50;
-  inputChatText.rows = 15;
-  inputChatText.resize = "none";
-  divChat.appendChild(inputChatText);
-
-  const inputChat = document.createElement('input');
-  inputChat.classList.add('detail__inputChat');
-  inputChat.type = "text";
-  inputChat.placeholder = "Escreva sua pergunta aqui.."
-  divChat.appendChild(inputChat);
-
   const item = dataMovie.find((movie) => movie.id === props.id);
 
-  const divDetail = renderCard(item);
-  divChat.appendChild(divDetail);
+  const divTemplateChat = document.createElement('div');
 
-  return divChat;
+  const sectionTemplateChat = document.createElement('section');
+  sectionTemplateChat.classList.add('grid-chat');
+  sectionTemplateChat.classList.add('grid-template-chat');
+  divTemplateChat.appendChild(sectionTemplateChat);
+
+  const divTituloChat = document.createElement('div');
+  divTituloChat.classList.add('item-chat');
+  divTituloChat.classList.add('titulo-chat');
+  sectionTemplateChat.appendChild(divTituloChat);
+  divTituloChat.textContent = `Iniciar Conversa`
+
+  const divDescricaoChat = document.createElement('div');
+  divDescricaoChat.classList.add('item-chat');
+  divDescricaoChat.classList.add('descricao-chat');
+  sectionTemplateChat.appendChild(divDescricaoChat);
+  divDescricaoChat.innerHTML = `<p class="description">${item.description}</p>`
+
+  sectionTemplateChat.appendChild(renderDetails(item));
+
+  const divLogoChat = document.createElement('div');
+  divLogoChat.classList.add('item-chat');
+  divLogoChat.classList.add('logo-chat');
+  sectionTemplateChat.appendChild(divLogoChat);
+  divLogoChat.innerHTML = `<img src="${item.imageUrl}" alt="Imagem do Filme" itemprop="${item.name}" class="image__card"/>`
+
+  const divTextareaChat = document.createElement('div');
+  divTextareaChat.classList.add('item-chat');
+  divTextareaChat.classList.add('textarea-chat');
+  sectionTemplateChat.appendChild(divTextareaChat);
+  divTextareaChat.innerHTML = `<textarea readOnly=true cols="61" rows="25" style="resize:none"></textarea>`;
+
+  const divInputChat = document.createElement('div');
+  divInputChat.classList.add('item-chat');
+  divInputChat.classList.add('input-chat');
+  sectionTemplateChat.appendChild(divInputChat);
+  divInputChat.innerHTML = `<input type="text" placeholder="Escreva aqui sua pergunta.."><input`;
+
+  return divTemplateChat;
 }
 
-export const renderCard = (item) => {
-  const divDetail = document.createElement('div');
-  divDetail.classList.add('detail__card');
-  divDetail.innerHTML = `
-        <dl itemscope itemtype="#">
-          <dt>
-            <img src="${item.imageUrl}" alt="Imagem do Filme" itemprop="${item.name}" class="image__card"/>
-          </dt>
+export const renderDetails = (item) => {
+  const divDetalhesChat = document.createElement('div');
+  divDetalhesChat.classList.add('item-chat');
+  divDetalhesChat.classList.add('detalhes-chat');
+  divDetalhesChat.innerHTML = `
           <dd itemprop="sort-order" class="imDbRating">${item.facts.imDbRating} /10 <img src="./img/icon-star.svg" alt="Star icon" /></dd>
           <dd itemprop="name" class="name__card">${item.name}</dd>
           <dd itemprop="releaseYear" class="releaseYear">${item.facts.releaseYear} - ${item.facts.runtimeStr}</dd>
           <dd itemprop="shortDescription" class="shortDescription">${item.shortDescription}</dd>
           <dd itemprop="movieGender" class="movieGender">${item.facts.movieGender}</dd>
-          <dd itemprop="directorsName" class="directors__card">${item.facts.directorsName}</dd>
-          <dd itemprop="movieStars" class="stars__card">${item.facts.movieStars}</dd>
-          <dd itemprop="description" class="description">${item.description}</dd>
-          <button class='btn-chatGrup'>Chat em grupo</button>
-        </dl>
-    `;
-  // div.addEventListener("click", () => navigateTo("/chatGrupo", { id: item.id }))
-
-  return divDetail;
-};
+          <dd itemprop="directorsName" class="directors__card">Diretores: ${item.facts.directorsName}.</dd>
+          <dd itemprop="movieStars" class="stars__card">Principais autores: ${item.facts.movieStars}.</dd>
+  `;
+  return divDetalhesChat;
+}
 
 export default Chat;

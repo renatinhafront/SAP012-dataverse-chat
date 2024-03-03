96559475d79e837ee4f3c6f623ec35ce5e13d587
renatinhafront <re.saraiva09@hotmail.com>
2024-03-02 21:23:46 -0300

    em criacao dos filmes onlines

--- src/views/ChatGroup.js
+++ src/views/ChatGroup.js
@@ -2,28 +2,50 @@ import dataMovie from '../data/dataset.js';
 
 export function ChatGroup() {
 
-  const divTemplateChatGroup = document.createElement('div');
+  const sectionTemplateChatGroup = document.createElement('section');
+  sectionTemplateChatGroup.classList.add('display__chat__group');
+  sectionTemplateChatGroup.classList.add('container__chat__group');
 
-  const sectionChatGroup = document.createElement('section');
-  sectionChatGroup.classList.add('titulo-chatGroup');
-  divTemplateChatGroup.appendChild(sectionChatGroup);
+  const divChatGroup = document.createElement('div');
+  divChatGroup.classList.add('item__chat__group');
+  sectionTemplateChatGroup.appendChild(divChatGroup);
 
-  const divTituloChatGroup = document.createElement('div');
-  divTituloChatGroup.classList.add('titulo-chatGroup');
-  sectionChatGroup.appendChild(divTituloChatGroup);
-  divTituloChatGroup.innerHTML = `
-      <h1>The Best Movies</h1>
-      <dd clas="">Conheça sobre os melhores filmes da história</dd>
-  `;
+  const divOnlineUsers = document.createElement('div');
+  divOnlineUsers.classList.add('item__chat__group');
+  divOnlineUsers.appendChild(userOnline());
+  sectionTemplateChatGroup.appendChild(divOnlineUsers);
 
-  // const item = dataMovie.find((movie) => movie.id === props.id);
+  // const divTituloChatGroup = document.createElement('div');
+  // divTituloChatGroup.classList.add('titulo-chatGroup');
+  // sectionTemplateChatGroup.appendChild(divTituloChatGroup);
+  // divTituloChatGroup.innerHTML = `
+  //     <h1>The Best Movies</h1>
+  //     <dd clas="">Conheça sobre os melhores filmes da história</dd>
+  //   `;
 
-  sectionChatGroup.appendChild(addComentario());
 
-  return divTemplateChatGroup;
+  // const sectionChatGroup = document.createElement('section');
+  // sectionChatGroup.classList.add('titulo-chatGroup');
+  // divTemplateChatGroup.appendChild(sectionChatGroup);
+
+  // const divTituloChatGroup = document.createElement('div');
+  // divTituloChatGroup.classList.add('titulo-chatGroup');
+  // sectionChatGroup.appendChild(divTituloChatGroup);
+  // divTituloChatGroup.innerHTML = `
+  //     <h1>The Best Movies</h1>
+  //     <dd clas="">Conheça sobre os melhores filmes da história</dd>
+  // `;
+
+  // // const item = dataMovie.find(item);
+  // dataMovie.forEach(movie => {
+  //   sectionChatGroup.appendChild(userOnline(movie));
+  //   sectionChatGroup.appendChild(addComentario(movie));
+
+  return sectionTemplateChatGroup;
 }
 
 export const addComentario = (item) => {
+
 
   const divTemplateComentario = document.createElement('div');
 
@@ -40,20 +62,39 @@ export const addComentario = (item) => {
   return divTemplateComentario;
 }
 
-export const userOnline = (itemGroup) => {
 
-  const divUserOnline = document.createElement('div');
+export const userOnline = () => {
+  const ul = document.createElement('ul');
+  ul.classList.add('container__user__online');
 
-  const sectionUserOnline = document.createElement('section');
-  sectionUserOnline.classList.add('comentario');
-  divUserOnline.appendChild(sectionUserOnline);
-  divUserOnline.innerHTML = `
-      <img src="${itemGroup.imageUrl}" alt="Imagem do Filme" itemprop="${itemGroup.name}" class="image__card"/>
-      <dd itemprop="name" class="name__card">${itemGroup.name}</dd>
+  dataMovie.forEach((item) => {
+    if (item.facts.isOnline) {
+      const li = document.createElement('li');
+      li.classList.add('container__user__online__item');
+      li.innerHTML = `
+      <section class="grid__user__online">
+        <div class="item_user_online logo_user_online">
+          <img src="${item.imageUrl}" alt="Imagem do Filme" class="image__user__online"/>
+        </div>
 
-  `;
+        <div class="item_user_online name_user_online">
+        <p class="font__medium">${item.name}</p>
+        </div>
 
-  return divUserOnline;
-}
+        <div class="item_user_online description_user_online">
+        <p class="description">${item.shortDescription}</p>
+        </div>
+      </section>
+      <hr/>
+    `;
+      ul.appendChild(li);
+    }
+  });
+
+  return ul;
+};
+
+
+
 
 export default ChatGroup;

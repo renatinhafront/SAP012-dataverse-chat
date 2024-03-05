33146359ad0d0f64447c413a1f20b343d8d0628e
renatinhafront <re.saraiva09@hotmail.com>
2024-03-05 14:17:53 -0300

    criado a função de comunicaçao com a ia

--- src/lib/openAIApi.js
+++ src/lib/openAIApi.js
@@ -1,9 +1,45 @@
-// src/lib/openAIApi.js
+//import { getApiKey } from "./apiKey";
 
-// Importa la función para obtener la API KEY desde apiKey.js
-// import { getApiKey } from './apiKey.js';
+const url = 'https://api.openai.com/v1/chat/completions';
+
+export const communicateWithOpenAI = (texto) => {
+  const apiKey = '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';//getApiKey();
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`
+    },
+    body: JSON.stringify({
+      messages: [
+        {
+          role: "user",
+          content: texto
+        }
+      ],
+      model: 'gpt-4',
+    })
+  };
+
+  return new Promise((resolve, reject) => {
+    fetch(url, requestOptions)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Erro ao chamar a API do OpenAI');
+        }
+        return response.json();
+      })
+      .then(data => {
+        resolve(data.choices[0].message.content.trim());
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+}
 
 
-// export const communicateWithOpenAI = (messages) => {
-//    //Aquí es donde debes implementar la petición con fetch o axios
-// };
+
+
+

8d8bc636b22d46ff0c2020a0472e16458193011c
laismicroni <laismicroni@gmail.com>
2024-03-07 11:32:27 -0300

    teste key

--- test/apiKey.spec.js
+++ test/apiKey.spec.js
@@ -1,37 +1,15 @@
-// test/apiKey.spec.js
-
 import { getApiKey, setApiKey } from '../src/lib/apiKey.js';
 
-describe('getApiKey', () => {
-  it('deve retornar o valor da chave da API armazenada', () => {
-    // Configuração
-    const apiKey = 'minhaChaveDeAPI';
-    localStorage.setItem('API_KEY', apiKey);
-    // Ação
-    const chaveAPIRecuperada = getApiKey();
-    // Verificação
-    expect(chaveAPIRecuperada).toBe(apiKey);
-    // Limpeza
-    localStorage.removeItem('API_KEY');
+const mockApiKey = "123abc"
+
+describe('ApiKey Functions', () => {
+
+  it('Deve definir corretamente o valor da chave da API', () => {
+    setApiKey(mockApiKey)
+    expect(localStorage.getItem("API_KEY")).toEqual(mockApiKey)
   });
 
-  it('deve retornar null se não houver nenhuma chave de API armazenada', () => {
-    // Ação
-    const chaveAPIRecuperada = getApiKey();
-    // Verificação
-    expect(chaveAPIRecuperada).toBeNull();
-  });
-});
-
-describe('setApiKey', () => {
-  it('deve definir corretamente a chave da API', () => {
-    // Configuração
-    const apiKey = 'minhaChaveDeAPI';
-    // Ação
-    setApiKey(apiKey);
-    // Verificação
-    expect(localStorage.getItem('API_KEY')).toBe(apiKey);
-    // Limpeza
-    localStorage.removeItem('API_KEY');
+  it('Deve devolver o valor da chave API', () => {
+    expect(getApiKey()).toEqual(mockApiKey);
   });
 });
